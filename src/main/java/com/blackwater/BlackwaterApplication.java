bde646673c3ee82659de05ca1abd6e48c7021143
Rafael Teixeira <rafael.amtx@gmail.com>
2019-07-07 21:17:57 -0300

    model implementation

---
+++ src/main/java/com/blackwater/BlackwaterApplication.java
@@ -0,0 +1,14 @@
+package com.blackwater;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlackwaterApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(BlackwaterApplication.class, args);
+    }
+
+}
